5c7e09c5474de9164ad26cb7ee5d28554ed7ca31
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-05-16 13:25:06 -0400

    Arreglando span fantasma

--- js/script.js
+++ js/script.js
@@ -10,49 +10,48 @@ function agregar(){
 
 	var cont = document.getElementById("contenedor"); // Donde imprimiremos las tareas
 
-	//Creando nuevos nodos
-	var nuevasTareas = document.createElement("div");
-	var textoNuevaTarea = document.createTextNode(tareas);
-	var elementoContenedor = document.createElement("span");
-
-	//Asignandole padres a los nodos
-	elementoContenedor.appendChild(textoNuevaTarea);
-	nuevasTareas.appendChild(elementoContenedor);
-	cont.appendChild(nuevasTareas);
-
-	//CheckBox
-	var chck = document.createElement("input");
-	chck.type = "checkbox";
-	chck.setAttribute("class", "check");
-
-	var eliminar = document.createElement("span");
-	eliminar.classList.add("fa", "fa-trash-o");	//icono de basurero
-	var corazon = document.createElement("span");
-	corazon.classList.add("fa", "fa-heart"); 	//icono de corazon
-
-	//Asignando los nodos creados
-	nuevasTareas.appendChild(chck);
-	nuevasTareas.appendChild(eliminar);
-	nuevasTareas.appendChild(corazon);
-
-	//Agregando Eventos
-	chck.addEventListener("click", function(){
-		elementoContenedor.classList.toggle("tachado");	
-	})
-	eliminar.addEventListener("click", function(){
-		cont.removeChild(nuevasTareas);
-	})
-
-	//Creando funcion click para likes del corazon
-	corazon.addEventListener("click", function(){
-		corazon.classList.toggle("red");
-	})
-
-	//Validando textarea
+		//Validando textarea
 	if (tareas == null || tareas.length == 0) {
 		alert("Error! debe ingresar tarea");
 		return false;
-	}
+	}else{	//Arreglando span fantasma
+		//Creando nuevos nodos
+		var nuevasTareas = document.createElement("div");
+		var textoNuevaTarea = document.createTextNode(tareas);
+		var elementoContenedor = document.createElement("span");
+	
+		//Asignandole padres a los nodos
+		elementoContenedor.appendChild(textoNuevaTarea);
+		nuevasTareas.appendChild(elementoContenedor);
+		cont.appendChild(nuevasTareas);
+	
+		//CheckBox
+		var chck = document.createElement("input");
+		chck.type = "checkbox";
+		chck.setAttribute("class", "check");
+	
+		var eliminar = document.createElement("span");
+		eliminar.classList.add("fa", "fa-trash-o");	//icono de basurero
+		var corazon = document.createElement("span");
+		corazon.classList.add("fa", "fa-heart"); 	//icono de corazon
+	
+		//Asignando los nodos creados
+		nuevasTareas.appendChild(chck);
+		nuevasTareas.appendChild(eliminar);
+		nuevasTareas.appendChild(corazon);
+	
+		//Agregando Eventos
+		chck.addEventListener("click", function(){
+			elementoContenedor.classList.toggle("tachado");	
+		})
+		eliminar.addEventListener("click", function(){
+			cont.removeChild(nuevasTareas);
+		})
+	
+		//Creando funcion click para likes del corazon
+		corazon.addEventListener("click", function(){
+			corazon.classList.toggle("red");
+		})}
 
 }
 
